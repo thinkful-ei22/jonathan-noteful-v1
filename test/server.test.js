e4f054ef829ae7a63d4062d0a38912ee4c665019
Jonathan Kim <jonkim204@gmail.com>
2018-06-29 12:11:27 -0700

    travis

---
+++ test/server.test.js
@@ -0,0 +1,85 @@
+'use strict';
+const app = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Reality check', function () {
+
+  it('true should be true', function () {
+    expect(true).to.be.true;
+  });
+
+  it('2 + 2 should equal 4', function () {
+    expect(2 + 2).to.equal(4);
+  });
+
+});
+
+describe('Express static', function () {
+
+  it('GET request "/" should return the index page', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+
+});
+
+describe('404 handler', function () {
+
+  it('should respond with 404 when given a bad path', function () {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then(res => {
+        expect(res).to.have.status(404);
+      });
+  });
+
+});
+
+describe('Return Array', function () {
+  it('should return default length', function () {
+    return chai.request(app)
+      .get('/api/notes')
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.length(10);
+      });
+  });
+  
+  it('should return objects with correct keys', function () {
+    return chai.request(app)
+      .get('/api/notes')
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.length(10);
+        res.body.forEach(function (item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys('id', 'title', 'content');
+        });
+      });
+  });
+});
+
+it('should return correct results', function () {
+  return chai.request(app)
+    .get('/api/notes?searchTerm=about%20cats')
+    .then(res => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      expect(res.body).to.have.length(4);
+      expect(res.body[0]).to.be.a('object');
+    });
+});
